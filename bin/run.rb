a5e57185d6908aaaa7d00723a8477af71e191b3f
andy-aguilar <aaguil3@gmail.com>
2020-06-16 15:34:53 -0400

    formatted welcome_message and list_commands

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,5 @@
 require_relative '../config/environment'
-# Dish.find(1).dish_to_screen
 
-# puts Dish.find_ingredients('lasagna')
-a = Dish.find_by_ingredients(["tomato sauce"])
-my_shopping_list(a)
-
-
-
+welcome_message
+list_commands
+get_user_input
--- lib/run_helper.rb
+++ lib/run_helper.rb
@@ -13,3 +13,33 @@ def my_shopping_list(dish_array)
         puts "- #{ingredient_name}"
     end
 end
+
+def list_commands
+    puts "".center(100, "-*")
+    puts " MENU ".center(100, "-*")
+    puts "".center(100, "-*")
+    puts "\n1. Find Dish: Finds a delicious meal based on the ingredients you have at home"
+    puts "\n2. Random Dish: Finds a completely random dish (Warning: only for the adventurous)"
+    puts "\n3. My Dishes: Lists recipes you've added to 'My Dishes.'"
+    puts "\n4. Shopping List: Gives you a list of all the ingredients you'll need to start making your dishes\n\n"
+    puts "What would you like to do?\n\n"
+end
+
+def welcome_message
+    puts "\n"
+    puts "".center(100, "-*")
+    puts " RECIFIND ".center(100, "-*")
+    puts " Welcome to Recifind: the word's best recipe finder! ".center(100, "-*")
+    puts " Type 'help' at anytime for a list of commands ".center(100, "-*")
+    puts "".center(100, "-*")
+    puts "".center(100, "-*")
+    puts "\n\n"
+
+end
+def get_user_input
+    user_input = gets.chomp()
+    #if user_input == help
+    #    list_commands
+    #if user_input.downcase == find dish
+    #    puts 
+end
